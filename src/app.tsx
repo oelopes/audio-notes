accb1ab27e837938b5bba25964d3d6324a65a5f7
oelopes <oelopes2@gmail.com>
2024-02-09 19:37:08 -0300

    refactor: create reusable note card component

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { NoteCard } from "./components/note-card"
+import { NewNoteCard, NoteCard } from "./components/note-card"
 
 export const App = () => {
   return (
@@ -14,11 +14,7 @@ export const App = () => {
       <div className="h-px bg-slate-700" />
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-        <div className="rounded-md bg-slate-700 p-5 space-y-3">
-          <span className="text-sm font-medium text-slate-500">Adicionar nota</span>
-
-          <p className="text-sm leading-6 text-slate-400">Grave uma nota em áudio que será convertida para texto automaticamente.</p>
-        </div>
+        <NewNoteCard />
 
         <NoteCard date={new Date()} content='blalbvasloa odsak dosakdoaskodka' />
       </div>
--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -3,19 +3,23 @@ import { formatDistanceToNow } from "date-fns"
 import { ptBR } from "date-fns/locale"
 import { X } from "lucide-react"
 
+type BaseNoteProps = {
+  children: React.ReactNode
+  actionButton: React.ReactNode
+  hasOverflow?: boolean
+}
+
 type NoteCardProps = {
   date: Date
   content: string
 }
 
-export const NoteCard = ({date, content}: NoteCardProps) => (
+export const BaseNote = ({children, actionButton, hasOverflow = false}: BaseNoteProps) => (
   <Dialog.Root>
     <Dialog.Trigger className="rounded-md flex flex-col text-left bg-slate-800 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-cyan-400">
-      <span className="text-sm font-medium text-slate-500">{formatDistanceToNow(date, { locale: ptBR, addSuffix: true})}</span>
+      {children}
 
-      <p className="text-sm leading-6 text-slate-400">{content}</p>
-
-      <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none"></div>
+      {hasOverflow && <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />}
     </Dialog.Trigger>
 
     <Dialog.Portal>
@@ -26,16 +30,46 @@ export const NoteCard = ({date, content}: NoteCardProps) => (
           </Dialog.Close>
 
           <div className="flex flex-1 flex-col gap-3 p-5">
-            <span className="text-sm font-medium text-slate-500">{formatDistanceToNow(date, { locale: ptBR, addSuffix: true})}</span>
-
-            <p className="text-sm leading-6 text-slate-400">{content}</p>
+            {children}
           </div>
 
-          <button className="w-full bg-slate-800 py-4 text-center text-sm-text-slate-300 outline-none font-medium group">
-            Deseja <span className="text-red-400 group-hover:underline">apagar essa nota?</span>
-          </button>
+          {actionButton}
         </Dialog.Content>
       </Dialog.Overlay>
     </Dialog.Portal>
   </Dialog.Root>
 )
+
+const DeleteButton = () => (
+  <button className="w-full bg-slate-800 py-4 text-center text-sm-text-slate-300 outline-none font-medium group">
+    Deseja <span className="text-red-400 group-hover:underline">apagar essa nota?</span>
+  </button>
+)
+
+export const NoteCard = ({date, content}: NoteCardProps) => {
+  return (
+  <BaseNote hasOverflow actionButton={<DeleteButton />}>
+    <span className="text-sm font-medium text-slate-500">{formatDistanceToNow(date, { locale: ptBR, addSuffix: true})}</span>
+
+    <p className="text-sm leading-6 text-slate-400">{content}</p>
+  </BaseNote>
+  )
+}
+
+const AddButton = () => (
+  <button className="w-full bg-cyan-400 py-4 text-center text-sm text-cyan-950 outline-none font-medium group hover:bg-cyan-500">
+    Salvar nota
+  </button>
+)
+
+export const NewNoteCard = () => {
+  return (
+    <BaseNote actionButton={<AddButton />}>
+      <span className="text-sm font-medium text-slate-500">Adicionar nota</span>
+
+      <p className="text-sm leading-6 text-slate-400">
+        Comece <button className="text-cyan-400 font-medium hover:underline">gravando uma nota</button> em áudio ou se preferir utilize <button className="text-cyan-400 font-medium hover:underline">apenas texto</button>
+      </p>
+    </BaseNote>
+  )
+}
