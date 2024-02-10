68800052d1572505e04f181efd7b1b4466f71ad8
oelopes <oelopes2@gmail.com>
2024-02-10 01:22:27 -0300

    feat: filter notes

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,21 @@
+import { ChangeEvent, useState } from "react"
 import { NewNoteCard, NoteCard } from "./components/note-card"
 
 import { useNotes } from "./contexts/NotesContext"
 
 export const App = () => {
   const {notes, handleNoteCreation, handleNoteDeletion} = useNotes()
+
+  const [search, setSearch] = useState('')
+
+  const handleSearch = (event: ChangeEvent<HTMLInputElement>) => {
+    const query = event.target.value
+
+    setSearch(query)
+  }
+
+  const filteredNotes = notes.filter((note) => note.content.toLowerCase().includes(search.toLowerCase()))
+  
 
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
@@ -12,6 +24,7 @@ export const App = () => {
           className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500" 
           type="text" 
           placeholder="Busque em suas notas..." 
+          onChange={handleSearch}
         />
       </form>
 
@@ -20,7 +33,7 @@ export const App = () => {
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard onNoteCreation={handleNoteCreation}/>
 
-        {notes.map((note) => (
+        {filteredNotes.map((note) => (
           <NoteCard key={note.id} data={note} onNoteDeletion={handleNoteDeletion} />
         ))}
       </div>
